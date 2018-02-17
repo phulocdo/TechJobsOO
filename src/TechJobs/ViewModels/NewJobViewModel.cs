155731036f27c0226144abdf01f565331636de7f
phulocdo <philip.loc.do@gmail.com>
2018-02-17 01:20:30 -0500

    create new jobs

--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -15,8 +15,21 @@ namespace TechJobs.ViewModels
         [Display(Name = "Employer")]
         public int EmployerID { get; set; }
 
+        [Required]
+        [Display(Name = "Location")]
+        public int LocationID { get; set; }
+
+        [Required]
+        [Display(Name = "Skill")]
+        public int CoreCompetencyID { get; set; }
+
+        [Required]
+        [Display(Name = "Postion Type")]
+        public int PositionTypeID { get; set; }
+
+
         // TODO #3 - Included other fields needed to create a job,
-        // with correct validation attributes and display names.
+        // with correct validation attributes and display names-DONE.
 
         public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
         public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
@@ -37,7 +50,36 @@ namespace TechJobs.ViewModels
             }
 
             // TODO #4 - populate the other List<SelectListItem> 
-            // collections needed in the view
+            // collections needed in the view -DONE
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach(CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+                
+            }
+
+            foreach(PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem()
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+           
 
         }
     }
